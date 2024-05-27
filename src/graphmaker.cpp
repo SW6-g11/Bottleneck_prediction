04d8dca75128bf67292af0c747482e39453eba33
ThomasNyk <nykjaerthomas+github@gmail.com>
2024-05-27 13:46:06 +0200

    .

--- src/graphmaker.cpp
+++ src/graphmaker.cpp
@@ -1,39 +1,50 @@
 #include "graphmaker.h"
-#include "Sheetreader.h"
-#include "Linkutils.h"
-#include "Traffic.h"
-#include "Path.h"
-#include "Router.h"
-#include "Link.h"
-#include "graphDataStruct.h"
+#include <mainwindow.h>
 #include <iostream>
 #include <vector>
-#include <mainwindow.h>
+#include "Link.h"
+#include "Linkutils.h"
+#include "Path.h"
+#include "Router.h"
+#include "Sheetreader.h"
+#include "Traffic.h"
+#include "graphDataStruct.h"
 
 using namespace std;
 
-void Graphmaker::processDataForDay(string &directoryPath, int day, int Limit, graphDataStruct &graphdata)
-{
-    SheetReader Readmaster(Limit);
-    MainWindow &mainWindow = MainWindow::getInstance();
+void Graphmaker::processDataForDay(string& directoryPath,
+                                   int day,
+                                   int Limit,
+                                   graphDataStruct& graphdata) {
+  SheetReader Readmaster(Limit);
+  MainWindow& mainWindow = MainWindow::getInstance();
 
-    string linkUtilsFileName = directoryPath + "/link-util-day" + to_string(day) + ".csv";
-    Readmaster.readData(linkUtilsFileName, graphdata.linkUtilsData, false);
-    mainWindow.simulateProcessingOne();
+  graphdata.Augmentedlinks.clear();
+  graphdata.linkUtilsData.clear();
+  graphdata.MappedRouterVector.clear();
+  graphdata.pathsData.clear();
+  graphdata.trafficData.clear();
 
-    string flowsFileName = directoryPath + "/flow-traffic-day" + to_string(day) + ".csv";
-    Readmaster.readData(flowsFileName, graphdata.trafficData, false);
-    mainWindow.simulateProcessingOne();
+  string linkUtilsFileName =
+      directoryPath + "/link-util-day" + to_string(day) + ".csv";
+  Readmaster.readData(linkUtilsFileName, graphdata.linkUtilsData, false);
+  mainWindow.simulateProcessingOne();
 
-    string pathsFileName = directoryPath + "/flow-paths-day" + to_string(day) + ".csv";
-    Readmaster.readData(pathsFileName, graphdata.pathsData, false);
-    mainWindow.simulateProcessingOne();
+  string flowsFileName =
+      directoryPath + "/flow-traffic-day" + to_string(day) + ".csv";
+  Readmaster.readData(flowsFileName, graphdata.trafficData, false);
+  mainWindow.simulateProcessingOne();
 
-    string routersFileName = directoryPath + "/routers.csv";
-    Readmaster.readData(routersFileName, graphdata.MappedRouterVector, false);
-    mainWindow.simulateProcessingOne();
+  string pathsFileName =
+      directoryPath + "/flow-paths-day" + to_string(day) + ".csv";
+  Readmaster.readData(pathsFileName, graphdata.pathsData, false);
+  mainWindow.simulateProcessingOne();
 
-    string linksFileName = directoryPath + "/links.csv";
-    Readmaster.readData(linksFileName, graphdata.Augmentedlinks, false);
-    mainWindow.simulateProcessingOne();
+  string routersFileName = directoryPath + "/routers.csv";
+  Readmaster.readData(routersFileName, graphdata.MappedRouterVector, false);
+  mainWindow.simulateProcessingOne();
+
+  string linksFileName = directoryPath + "/links.csv";
+  Readmaster.readData(linksFileName, graphdata.Augmentedlinks, false);
+  mainWindow.simulateProcessingOne();
 };
