4a8a669bb25d3813d742efa7cee572004b5d242f
WiredG1 <thomas.nykjaer@live.dk>
2024-05-28 14:46:59 +0200

    optional fixes

--- include/graphviz.h
+++ include/graphviz.h
@@ -21,17 +21,17 @@ public:
     static void GenerateDotandPNGFile(const string &filename, bool usePreLoad, bool useTraffic, bool mincut, std::optional<std::string> result = nullopt, std::optional<vector<std::string>> mincutResult = nullopt);
 
 private:
-    static void writeRouters(ofstream &dotFile, vector<MappedRouter> &routervector, std::optional<string> minCutTarget);
+    static void writeRouters(ofstream &dotFile, vector<MappedRouter> &routervector, std::optional<string> minCutTarget = nullopt);
     static std::string getRouterColor(const MappedRouter &router, std::unordered_map<string, string> colors, std::optional<string> minCutTarget = nullopt);
-    static void writeTraffic(std::ofstream &dotFile, const std::vector<AugmentedLink> &linksData, bool usePreLoad, bool PeaksetOnly, std::optional<std::string> result);
+    static void writeTraffic(std::ofstream &dotFile, const std::vector<AugmentedLink> &linksData, bool usePreLoad, bool PeaksetOnly, std::optional<std::string> result = nullopt);
     // static void writePaths(ofstream& dotFile, const vector<Paths>& pathsData);
     // static void writeLinkUtils(ofstream& dotFile, const vector<Linkutils>& linkUtilsData);
-    static void writeLinks(ofstream &dotFile, const vector<AugmentedLink> &linksData, std::optional<std::string> result);
+    static void writeLinks(ofstream &dotFile, const vector<AugmentedLink> &linksData, std::optional<std::string> result = nullopt);
     static void GenerateImageFromDotFile(string dotFilename);
     static std::string convertDotToPngFilename(const std::string &dotFilename);
     static void InvertDotFile(const std::string &dotFilename);
     static std::string doubleToString(double db);
-    static void writeMincut(std::ofstream &dotFile, const std::vector<AugmentedLink> &linksData, std::optional<std::string> result, std::optional<vector<std::string>> mincutResult);
-    static std::string getRightColor(const AugmentedLink &link, std::optional<std::string> result, unordered_map<std::string, std::string>, std::optional<vector<string>> minCut = nullopt);
+    static void writeMincut(std::ofstream &dotFile, const std::vector<AugmentedLink> &linksData, std::optional<std::string> result = nullopt, std::optional<vector<std::string>> mincutResult = nullopt);
+    static std::string getRightColor(const AugmentedLink &link, std::optional<std::string> result = nullopt, unordered_map<std::string, std::string>, std::optional<vector<string>> minCut = nullopt);
 };
 #endif // GRAPHVIZ_H
